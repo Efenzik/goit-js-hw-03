1a198f08d71d6312858d9a46ca89b8da867f73be
Artem Cherkashin <157517719+Efenzik@users.noreply.github.com>
2025-03-25 16:30:14 +0200

    ,

--- js/task-3.js
+++ js/task-3.js
@@ -1,39 +1,39 @@
-const findBestEmployee = function (employees) {
-  let bestEmployee = "";
-  let maxExams = 0;
+// const findBestEmployee = function (employees) {
+//   let bestEmployee = "";
+//   let maxExams = 0;
 
-  for (const employer in employees) {
-    if (employees[employer] > maxExams) {
-      maxExams = employees[employer];
-      bestEmployee = employer;
-    }
-  }
+//   for (const employer in employees) {
+//     if (employees[employer] > maxExams) {
+//       maxExams = employees[employer];
+//       bestEmployee = employer;
+//     }
+//   }
 
-  return bestEmployee;
-};
+//   return bestEmployee;
+// };
 
-console.log(
-  findBestEmployee({
-    ann: 29,
-    david: 35,
-    helen: 1,
-    lorence: 99,
-  })
-); // lorence
+// console.log(
+//   findBestEmployee({
+//     ann: 29,
+//     david: 35,
+//     helen: 1,
+//     lorence: 99,
+//   })
+// ); // lorence
 
-console.log(
-  findBestEmployee({
-    poly: 12,
-    mango: 17,
-    ajax: 4,
-  })
-); // mango
+// console.log(
+//   findBestEmployee({
+//     poly: 12,
+//     mango: 17,
+//     ajax: 4,
+//   })
+// ); // mango
 
-console.log(
-  findBestEmployee({
-    lux: 147,
-    david: 21,
-    kiwi: 19,
-    chelsy: 38,
-  })
-); // lux
+// console.log(
+//   findBestEmployee({
+//     lux: 147,
+//     david: 21,
+//     kiwi: 19,
+//     chelsy: 38,
+//   })
+// ); // lux
