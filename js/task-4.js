54996e05953565524ced048bb92da30a94f3667c
Artem Cherkashin <157517719+Efenzik@users.noreply.github.com>
2025-03-25 16:31:14 +0200

    ,

--- js/task-4.js
+++ js/task-4.js
@@ -1,27 +1,27 @@
-// const countTotalSalary = function (employees) {
-//   let totalSalary = 0;
+const countTotalSalary = function (employees) {
+  let totalSalary = 0;
 
-//   for (const employee in employees) {
-//     totalSalary += employees[employee];
-//   }
+  for (const employee in employees) {
+    totalSalary += employees[employee];
+  }
 
-//   return totalSalary;
-// };
+  return totalSalary;
+};
 
-// console.log(countTotalSalary({})); // 0
+console.log(countTotalSalary({})); // 0
 
-// console.log(
-//   countTotalSalary({
-//     mango: 100,
-//     poly: 150,
-//     alfred: 80,
-//   })
-// ); // 330
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
 
-// console.log(
-//   countTotalSalary({
-//     kiwi: 200,
-//     lux: 50,
-//     chelsy: 150,
-//   })
-// ); // 400
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
